0da5427640ba3b8c9e9fa042a7fc6dd7542c9e97
zhengjiatong <940188085@qq.com>
2020-10-16 21:17:39 +0800

    testCsharp

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ServiceStack.Redis;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        class RedisUtils
+        {
+            public RedisClient redis_client;
+            public RedisUtils(string host, int queueport, string password)
+            {
+                // Console.WriteLine($"Host:{config.redis_host},Port:{config.redis_port_queue},Password:{config.redis_pass}");
+                //redis_client = new RedisClient("h.redis.sogou", 2335, "sogoufanyiapp2020");
+                redis_client = new RedisClient(host, queueport, password);
+            }
+            static void Main(string[] args)
+            {
+                string redis_server = "h.redis.sogou";
+                string redis_port = "2335";
+                string redis_password = "sogoufanyiapp2020";
+                RedisUtils redis = new RedisUtils(redis_server, int.Parse(redis_port), redis_password);
+                while (true)
+                {
+                    string one_json = redis.redis_client.PopItemFromList("testDocTranslateFileJsonList");
+                }
+            }
+        }
+    }
+}
